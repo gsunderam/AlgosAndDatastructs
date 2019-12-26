69c86b232f48d2cd97f9782459379cda4e38c7e6
gsunderam <gsunderam@rediffmail.com>
2019-12-25 20:11:46 -0700

    Added recursive sort

--- src/main/java/com/gs/home/datastructures/Heap.java
+++ src/main/java/com/gs/home/datastructures/Heap.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
  */
 public class Heap {
 	private Integer[] heapData;
-	private int currentPosition = -1;
+	private static int currentPosition = -1;
 	
 	public Heap(int capacity) {
 		this.heapData = new Integer[capacity];
@@ -37,13 +37,13 @@ public class Heap {
 				int childToSwap;
 				if (rightChild > upto) {//if right child index > size - 1, then just take the left child
 					childToSwap = leftChild;
-					stdout("End of heap reached: RC: " + rightChild);
+//					stdout("End of heap reached: RC: " + rightChild);
 				} else {
 					childToSwap = (heapData[leftChild] > heapData[rightChild]) ? leftChild : rightChild;
 				}
-				
+
 				if (heapData[index] < heapData[childToSwap]) {
-					stdout("Swapping data at " + index + " with " + childToSwap);
+//					stdout("Swapping data at " + index + " with " + childToSwap);
 					swap(childToSwap, index);
 				} else {
 					break;
@@ -75,11 +75,20 @@ public class Heap {
 	 */
 	public void heapSort() {
 		for (int i=0; i < currentPosition; i++) {
-			int tmp = heapData[0]; // max element
-			heapData[0] = heapData[currentPosition-i]; // bring last element to the root
-			heapData[currentPosition-i] = tmp; // put max at the last of unsorted part
+//			int tmp = heapData[0]; // max element
+//			heapData[0] = heapData[currentPosition-i]; // bring last element to the root
+//			heapData[currentPosition-i] = tmp; // put max at the last of unsorted part
+			swap(0, currentPosition - i);
 			fixDown(0, currentPosition-i-1);
 		}
+	}
+
+	public void recursiveSort(int index) {
+		if (index == currentPosition) return; //At this point, heap is sorted. (index = len - 1)
+
+		swap(0, currentPosition - index);
+		fixDown(0, currentPosition - index - 1);
+		recursiveSort(index + 1);
 	}
 	
 	@Override
@@ -89,13 +98,14 @@ public class Heap {
 	
 	//This builds a heap from the given data array
 	public static Heap heapify(int[] data) {
+		resetCurrentPosition();
 		Heap heap = new Heap(data.length);
 		for (int i=0; i < data.length; i++) {
 			heap.insert(data[i]);
 		}
 		return heap;
 	}
-	
+
 	private void swap(int index, int i) {
 		int tmp = heapData[i];
 		heapData[i] = heapData[index];
@@ -105,25 +115,30 @@ public class Heap {
 	private int size() {
 		return currentPosition + 1;
 	}
+
+	private static void resetCurrentPosition() {
+		currentPosition = -1;
+	}
 	
 	public static void main(String[] args) {
 		Heap heap = new Heap(10);
-		heap.insert(10);
-		heap.insert(15);
-		heap.insert(27);
-		heap.insert(5);
+		heap.insert(8);
+		heap.insert(1);
+		heap.insert(4);
+		heap.insert(6);
+		heap.insert(11);
+		heap.insert(9);
 		heap.insert(2);
-		heap.insert(21);
-		heap.insert(14);
-		stdout("Size of the heap: " + heap.size());
+		stdout("Size of the heap: " + heap.size() + " current pos: " + currentPosition);
 		stdout(heap);
 		stdout(heap.deleteRoot());
 		stdout(heap);
-		heap.heapSort();
-		stdout(heap);
 		stdout("Size of the heap: " + heap.size());
-//		Heap another = Heap.heapify(new int[]{73,16,40,1,46,28,12,21,22,44,66,90,7});
-//		another.heapSort();
-//		System.out.println(another);
+		heap.recursiveSort(0);
+		stdout(heap);
+
+		Heap another = Heap.heapify(new int[] {73,16,40,1,46,28,12,21,22,44,66,90,7});
+		another.recursiveSort(0);
+		stdout(another);
 	}
 }
